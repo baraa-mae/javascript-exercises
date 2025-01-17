9f8970f45c6a9a19bf7ab4ae2b93e2ff060945a6
Baraa <albaraamae@gmail.com>
2025-01-17 22:58:08 +0200

    Solve the first 7 exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let DivisibleByFour = year % 4 == 0;
+  let isCentury = year % 100 == 0;
+  let DivisibleByFourHundred = year % 400 == 0;
+  if (DivisibleByFour && (!isCentury || DivisibleByFourHundred)) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
